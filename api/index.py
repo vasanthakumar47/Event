cb1cb2fa198cab0157af5afdf6ad85cfba42bf95
MC-PM-2023 <mc-pm@datasolve-analytics.com>
2025-09-15 15:31:43 +0530

    chor(ui):vercel_v1

--- api/index.py
+++ api/index.py
@@ -1,4 +1,3 @@
-# api/index.py
-# Vercel auto-detects a WSGI app if you expose `app` (or `application`)
 from app import app as app
-application = app  # export both names for safety
+application = app
+
